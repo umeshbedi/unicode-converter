24718855ec03ebb5d4eb8ecf5b0a818511267b71
umeshbedi <ashramumesh@gmail.com>
2026-01-02 07:03:32 +0530

    deleting language done

--- preload.js
+++ preload.js
@@ -4,5 +4,6 @@ contextBridge.exposeInMainWorld('api', {
   listLanguages: () => ipcRenderer.invoke('list-languages'),
   loadMappings: (lang) => ipcRenderer.invoke('load-mappings', lang),
   saveMapping: (lang, from, to) => ipcRenderer.invoke('save-mapping', lang, from, to),
-  saveMappings: (lang, mappings) => ipcRenderer.invoke('save-mappings', lang, mappings)
+  saveMappings: (lang, mappings) => ipcRenderer.invoke('save-mappings', lang, mappings),
+  deleteLanguage: (lang) => ipcRenderer.invoke('delete-language', lang)
 });
